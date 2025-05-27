08feba9c6b8836d10c60bf569ff2f693f3617ec5
ritesh-joshi-dev <riteshjoshidev@gmail.com>
2025-05-27 20:40:19 +0530

    floating point division added

--- practice goprog/calc.go
+++ practice goprog/calc.go
@@ -27,4 +27,8 @@ func main() {
 	// % modulo-divides two variables
 	remainder := num1 % num2
 	fmt.Printf("%d %% %d = %d\n", num1, num2, remainder)
+
+	// / divide two floating point variables
+	result := float32(num1) / float32(num2)
+	fmt.Printf(" Floaating point division of %d / %d = %g\n", num1, num2, result)
 }
