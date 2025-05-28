81ca6f9631fb5cdbcca37b515654cf71878eb91d
ritesh-joshi-dev <riteshjoshidev@gmail.com>
2025-05-28 18:42:26 +0530

    empty interface uses

---
+++ practice goprog/emptyinterface.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func displayValue(i ...interface{}) {
+	fmt.Println(i)
+}
+
+func main() {
+
+	a := "Welcome to Programiz"
+	b := 20
+	c := false
+
+	displayValue(a)
+
+	displayValue(b)
+
+	displayValue(c)
+
+	displayValue(a, b)
+
+	displayValue(b, c)
+
+	displayValue(c, a)
+
+	displayValue(a, b, c)
+
+}
