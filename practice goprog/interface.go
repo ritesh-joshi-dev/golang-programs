35854b59237dd190b1e3d296b6ec64663153f14d
ritesh-joshi-dev <riteshjoshidev@gmail.com>
2025-05-28 18:27:30 +0530

    multiple interface

--- practice goprog/interface.go
+++ practice goprog/interface.go
@@ -7,28 +7,43 @@ type Shape interface {
 	area() float32
 }
 
-// struct to implement interface
+// Rectangle struct implements the interface
 type Rectangle struct {
 	length, breadth float32
 }
 
-// use struct to implement area() of interface
+// Rectangle provides implementation for area()
 func (r Rectangle) area() float32 {
 	return r.length * r.breadth
 }
 
+// Triangle struct implements the interface
+type Triangle struct {
+	base, height float32
+}
+
+// Triangle provides implementation for area()
+func (t Triangle) area() float32 {
+	return 0.5 * t.base * t.height
+}
+
 // access method of the interface
-func calculate(s Shape) {
-	fmt.Println("Area:", s.area())
+func calculate(s Shape) float32 {
+	return s.area()
 }
 
 // main function
 func main() {
 
 	// assigns value to struct members
-	rect := Rectangle{7, 4}
+	r := Rectangle{7, 4}
+	t := Triangle{8, 12}
 
 	// call calculate() with struct variable rect
-	calculate(rect)
+	rectangleArea := calculate(r)
+	fmt.Println("Area of Rectangle:", rectangleArea)
+
+	triangleArea := calculate(t)
+	fmt.Println("Area of Triangle:", triangleArea)
 
 }
